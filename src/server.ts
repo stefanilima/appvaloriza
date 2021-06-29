622939d522a702e9dfc803ea29c7cadcda1bc41c
stefanilima <stefanisilvadelima@hotmail.com>
2021-06-28 22:49:48 -0300

    Ajustes da aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,28 @@
-import express from "express";
+import "reflect-metadata";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
-app.get("/test", (request, response) => {
-    return response.send("Hello word!");
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/test-post", (request, response) => {
-    return response.send("Teste de um metodo post");
-});
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Aplicação iniciada"));
