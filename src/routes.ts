10d4677b38be206b8b701ae0bbeeeb281f3683e5
stefanilima <stefanisilvadelima@hotmail.com>
2021-07-19 23:34:50 -0300

    Criado o controller do compliments e adicionado uma rota

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,18 @@ import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentsController } from "./controllers/CreateComplimentsController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentsController = new CreateComplimentsController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
 router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentsController.handle);
 
 export { router };
